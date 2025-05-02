4df1d4e322a7ee55858114e6e4ebf33f7001b8bd
pavan77749 <pavangpay05@gmail.com>
2025-05-02 23:16:43 +0530

    feat: implement user routes and friend request functionality

---
+++ backend/src/routes/user.route.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import {protectRoute} from '../middleware/auth.middleware.js';
+import { getMyFriends, getRecommendatedUsers, sendFriendRequest,acceptFriendRequest,getFriendRequests,getOutgoingFriendRequests } from '../controllers/user.controller.js';    
+
+const router = express.Router();
+
+router.use(protectRoute)
+
+router.get('/',getRecommendatedUsers);
+router.get('/friends',getMyFriends);
+router.post('/friend-request/:id',sendFriendRequest);
+router.post('/friend-request/:id/accept', acceptFriendRequest); 
+router.get('/friend-requests', getFriendRequests); 
+router.get('/outgoing-friend-requests', getOutgoingFriendRequests);
+
+export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import userRoutes from './routes/user.route.js';
 
 
 const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
+app.use("/api/user", userRoutes);
 
 
 app.listen(PORT, () => {
