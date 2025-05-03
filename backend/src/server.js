e89d7af43b3b3f98d363a588ad3f39537eccd829
pavan77749 <pavangpay05@gmail.com>
2025-05-03 10:35:49 +0530

    feat: update frontend dependencies and implement routing
    - Added @tanstack/react-query and axios for data fetching.
    - Removed unused App.css and react.svg files.
    - Refactored App.jsx to implement React Router for navigation.
    - Created new pages: HomePage, LoginPage, SignupPage, NotificationPage, ChatPage, CallPage, and OnboardingPage.
    - Integrated react-hot-toast for notifications.
    - Set up Tailwind CSS with daisyUI for styling.
    - Configured PostCSS for Tailwind CSS.
    - Created axios instance for API calls.

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import "dotenv/config";
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
@@ -12,10 +13,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials: true, // Access-Control-Allow-Credentials:true
+}));
 
 app.use("/api/auth",authRoutes);
 app.use("/api/user", userRoutes);
-app.use('api/chat', chatRoutes);
+app.use('/api/chat', chatRoutes);
 
 
 app.listen(PORT, () => {
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+     
+    </div>
+  )
+}
+
+export default HomePage
