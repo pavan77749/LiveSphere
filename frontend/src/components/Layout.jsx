6bb644f4665d199cd833f5c20c4de1fb891c376b
pavan77749 <pavangpay05@gmail.com>
2025-05-11 18:26:58 +0530

    fix: update layout styles for consistent height and overflow behavior

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 const Layout = ({ children ,showSidebar=false}) => {
   return (
-    <div className='min-h-screen' >
+    <div className='h-screen overflow-auto' >
       <div className="flex">
        {
         showSidebar && <Sidebar/>
