9db02be6f8e70e79783cac0c1d7d8992ffd9d0cd
pavan77749 <pavangpay05@gmail.com>
2025-05-11 18:53:09 +0530

    deployment: add livesphere package and update dependencies in frontend and backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,13 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js';
 import chatRoutes from './routes/chat.route.js';
+import path from 'path';
 
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(cors({
   origin:"http://localhost:5173",
@@ -23,6 +26,13 @@ app.use("/api/auth",authRoutes);
 app.use("/api/users", userRoutes);
 app.use('/api/chat', chatRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist"," index.html"));
+  }
+ );
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
